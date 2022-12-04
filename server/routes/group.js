0ce3f25cf119941c85789c8bb2d4f33f8bef06a9
huyiteng <yitengh2@illinois.edu~>
2022-12-04 17:44:30 -0600

    fix some bugs in group api

--- server/routes/group.js
+++ server/routes/group.js
@@ -78,6 +78,7 @@ module.exports = function (router) {
                 });
             }
         });
+    });
         
     GroupidRoute.get(function (req, res) {
         // get groups info
@@ -105,42 +106,3 @@ module.exports = function (router) {
     return router;
 }
 
-
-// var Group = require("../models/group.js");
-
-// module.exports = function(router) {
-//     router.route("/groups").post(function(req, res) {
-//         var group = new Group();
-//         if(req.body.GroupName == undefined) {
-//             return res.status(400).send({
-//                 message: "Group Name cannot be empty",
-//                 data: []
-//             });
-//         } else {
-//             group.GroupName = req.body.GroupName;
-//         }
-//         if(req.body.GroupMember == undefined || req.body.GroupMember.length < 2) {
-//             return res.status(400).send({
-//                 message: "Group Member cannot be less than 2",
-//                 data: []
-//             });
-//         } else {
-//             group.GroupMember = req.body.GroupMember;
-//         }
-//         group.save()
-//         .then(function(data) {
-//             return res.status(201).send({
-//                 message: "Group created",
-//                 data: data
-//             });
-//         })
-//         .catch(function(error) {
-//             return res.status(500).send({
-//                 message: "Server error",
-//                 data: error
-//             });
-//         });
-//     });
-
-//     return router;
-// }
