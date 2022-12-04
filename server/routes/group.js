00956f15cca84b92decc2c03d335377fa63cc698
huyiteng <yitengh2@illinois.edu~>
2022-12-04 17:41:08 -0600

    create put method for group

--- server/routes/group.js
+++ server/routes/group.js
@@ -37,6 +37,48 @@ module.exports = function (router) {
         });
     });
 
+    GroupRoute.put(function (req, res) {
+        //  invite friends into groups or change groups' name
+        //  只修改群名 -> userid留空
+        //  只邀请成员 -> group name 也需要赋值 
+        var token = req.body.token;
+        var groupid = req.body.GroupID;
+        var userid = req.body.UserID;
+        var name = req.body.GroupName;    
+        var member = null;
+        Group.findById(groupid, function (err, gp) {
+            if (gp == null){
+                return res.status(404).json({
+                    message: "group not found",
+                    data: []
+                });
+            }
+            if (name == null) {
+                return res.status(500).json({
+                    message: "group name should not be null",
+                    data: []
+                });
+            }
+            member = gp.GroupMember;
+            if ( member.findIndex(userid) != -1) {
+                // already in the group
+                return res.status(500).json({
+                    message: "user already in the group!",
+                    data: []
+                });
+            } 
+            else {
+                // insert user into the GroupMember
+                gp.GroupMember.push(userid);
+                gp.GroupName = name;
+                gp.save();
+                return res.status(200).json({
+                    message: "group name updated!",
+                    data: []
+                });
+            }
+        });
+        
     GroupidRoute.get(function (req, res) {
         // get groups info
         Group.findById(req.params.id).exec()
