81932f0caf9743fac099c419a1c02c6604788d8a
Oscar <47160475+ARBALEST2018@users.noreply.github.com>
2022-12-10 10:34:15 -0800

    Add user login, post, get in client

--- client/src/utils/userData.ts
+++ client/src/utils/userData.ts
@@ -1,1 +1,52 @@
-import { SERVER_DOMAIN } from "./connection";
+import { SERVER_DOMAIN, Token } from "./connection";
+
+export async function getUserInfo(userId: String, token: Token) {
+    if (!token) {
+        return {};
+    }
+    const url = `${SERVER_DOMAIN}/user/${userId}`;
+    const response = await fetch(url, {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: token }),
+    });
+    const json = await response.json();
+    return json;
+}
+
+export async function loginUser(email: String, password:string) {
+    const url = `${SERVER_DOMAIN}/user/login`;
+    const response = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ Email: email, Password: password }),
+    });
+    const json = await response.json();
+    return json;
+}
+
+export async function createUser(email: String, password:string, username: string) {
+    const url = `${SERVER_DOMAIN}/user`;
+    const response = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({Email: email, Password: password, UserName: username}),
+    });
+    const json = await response.json();
+    return json;
+}
