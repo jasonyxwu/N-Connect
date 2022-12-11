02caf2da9e3861ca81a8358883ce19aa74392999
jasonwu <admwyx@foxmail.com>
2022-12-11 15:46:13 -0500

    fixed conflicts

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Token } from "../utils/connection";
+import { Token } from "../utils/global";
 import { Provider } from "react-redux";
 import store from "../store";
 
