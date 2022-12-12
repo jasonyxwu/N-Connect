5b027c7e8ec219beb8e79904367074a2e51f466f
Oscar <47160475+ARBALEST2018@users.noreply.github.com>
2022-12-11 17:53:21 -0800

    Add searchUsers in client userData.ts

--- client/src/utils/userData.ts
+++ client/src/utils/userData.ts
@@ -19,6 +19,25 @@ export async function getUserInfo(userId: String, token: Token) {
     return json;
 }
 
+export async function searchUsers(UserName: String, token: Token) {
+    if (!token) {
+        return {};
+    }
+    const url = `${SERVER_DOMAIN}/users`;
+    const response = await fetch(url, {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: token, UserName: UserName}),
+    });
+    const json = await response.json();
+    return json;
+}
+
 export async function loginUser(email: String, password: string) {
     const url = `${SERVER_DOMAIN}/user/login`;
     const response = await fetch(url, {
