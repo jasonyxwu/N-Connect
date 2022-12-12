2304d9dfadcbd88bc9fb28e2ecde4bfe20402160
Oscar <47160475+ARBALEST2018@users.noreply.github.com>
2022-12-11 16:58:49 -0800

    Add username keyword search

--- server/routes/user.js
+++ server/routes/user.js
@@ -315,6 +315,63 @@ module.exports = function(router) {
             }
         }
     });
+
+    router.route("/users").get(function(req, res) {
+        // req.body.token = {
+            // id: String,
+            // Email: String 
+        // }
+        // Token verification start
+        if (req.body.token == undefined) {
+            return res.status(404).send({
+                message: "No valid token",
+                data: []
+            });
+        } else {
+            if (req.body.token.id == undefined) {
+                return res.status(404).send({
+                    message: "No valid token id",
+                    data: []
+                });
+            } else if (req.body.token.Email == undefined) {
+                return res.status(404).send({
+                    message: "No valid token Email",
+                    data: []
+                });
+            } else {
+                User.findById(req.body.token.id).exec()
+                .then(function(user) {
+                    if(user == null) {
+                        return res.status(404).send({
+                            message: "Invalid token id",
+                            data: [{"InvalidTokenID":req.body.token.id}]
+                        });
+                    } else if(req.body.token.Email != user.Email) {
+                        return res.status(404).send({
+                            message: "Token Email does not match id",
+                            data: [{"InvalidTokenEmail":req.body.token.Email}]
+                        });
+                    } else {
+                        // Token verification end
+                        User.find({UserName:/req.body.UserName/i}).exec()
+                        .then(function(users) {
+                            return res.status(200).send({
+                                message: "Users found",
+                                data: users
+                            });
+                        })
+                        
+                    }
+                })
+                .catch(function(error) {
+                    return res.status(500).send({
+                        message: "Server error",
+                        data: error
+                    });
+                });
+            }
+        }
+    });
     
 
     // router.route("/user/:id").delete(function(req, res) {
